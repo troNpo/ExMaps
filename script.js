9b3c76b49b53440ae126e22686535afa76ec2bff
troNpo <97335264+troNpo@users.noreply.github.com>
2025-09-02 11:02:50 +0200

    Update script.js
    Se añade escala

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const map = L.map('map', {
   zoomControl: false
 }).setView([37.3886, -5.9953], 13);
-
+L.control.scale({ imperial: false }).addTo(map);
 
 map.on("moveend", () => {
   requestAnimationFrame(() => {
